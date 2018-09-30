f0a65ed191f9b35dccfefdf3c13ba37075e51974
Tomas Kubica <tkubica12@gmail.com>
2018-09-30 20:28:47 +0200

    web container

---
+++ web/version1/web.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+   "github.com/go-martini/martini"
+   "os/exec"
+)
+
+func main() {
+  m := martini.Classic()
+
+  uuid, err := exec.Command("uuidgen").Output()
+  if err != nil {
+    panic("Unable to generate uuid")
+  }
+
+  m.Get("/", func() string {
+    return "<h1>Welcome to Version 1<br><br>This is server " + string(uuid) + "</h1>"
+  })
+
+  m.Run()
+}
