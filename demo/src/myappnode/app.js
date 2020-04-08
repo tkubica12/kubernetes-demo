b278451482f08f7098df2fef2fb932d6bd0f811a
Tomas Kubica <tkubica12@gmail.com>
2020-04-08 15:31:38 +0200

    fix app call

--- demo/src/myappnode/app.js
+++ demo/src/myappnode/app.js
@@ -3,6 +3,7 @@ const http = require('http')
 const { ServiceBusClient } = require("@azure/service-bus"); 
 const connectionString = process.env.SERVICEBUS_TODO_CONNECTION;
 const queueName = process.env.QUEUE_NAME; 
+const url = process.env.URL; 
 const app = express()
 const port = 3000
 
@@ -13,22 +14,22 @@ const message= {
     body: `Message sent`,
     label: `test`
   };
-const options = {
-    hostname: process.env.TARGET_HOST,
-    port: process.env.TARGET_PORT,
-    path: '/api/todo',
-    method: 'GET'
-  }
 
-app.get('/api/node', function(req, res) {
+app.get('/api/node', async function(req, res) {
     console.log('Calling todo API...');
-    const apireq = http.request(options, apires => {
-        console.log(`statusCode: ${apires.statusCode}`)
-      
-        apires.on('data', d => {
-          process.stdout.write(d)
-        })
+    http.get(url, (resp) => {
+      let data = '';
+      resp.on('data', (chunk) => {
+        data += chunk;
       });
+    
+      resp.on('end', () => {
+        console.log(data);
+      });
+    
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
     console.log('Sending message...');
     sender.send(message);
     res.send('Sent');
