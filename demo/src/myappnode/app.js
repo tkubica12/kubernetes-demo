e4697410a3b7f06c044be58823a38a5f16b7ddb1
Tomas Kubica <tkubica12@gmail.com>
2020-04-08 11:50:18 +0200

    todo-node swithc to http call

--- demo/src/myappnode/app.js
+++ demo/src/myappnode/app.js
@@ -1,20 +1,19 @@
 const express = require('express')
-const https = require('https')
+const http = require('http')
 const { ServiceBusClient } = require("@azure/service-bus"); 
 const connectionString = process.env.SERVICEBUS_TODO_CONNECTION;
 const queueName = process.env.QUEUE_NAME; 
 const app = express()
 const port = 3000
 
-const sbClient = ServiceBusClient.createFromConnectionString(connectionString); 
-const queueClient = sbClient.createQueueClient(queueName);
-const sender = queueClient.createSender();
+// const sbClient = ServiceBusClient.createFromConnectionString(connectionString); 
+// const queueClient = sbClient.createQueueClient(queueName);
+// const sender = queueClient.createSender();
 const message= {
     body: `Message sent`,
     label: `test`
   };
 const options = {
-  //  hostname: 'todo-myapp-todo',
     hostname: 'todo-myapp-todo',
     port: 8080,
     path: '/api/todo',
@@ -23,7 +22,7 @@ const options = {
 
 app.get('/api/node', function(req, res) {
     console.log('Calling todo API...');
-    const apireq = https.request(options, apires => {
+    const apireq = http.request(options, apires => {
         console.log(`statusCode: ${apires.statusCode}`)
       
         apires.on('data', d => {
@@ -31,7 +30,7 @@ app.get('/api/node', function(req, res) {
         })
       });
     console.log('Sending message...');
-    sender.send(message);
+    // sender.send(message);
     res.send('Sent');
   });
 
