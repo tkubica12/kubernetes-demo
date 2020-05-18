5f71561ecae9eabe3e597876dbb70656505942f6
Tomas Kubica <tkubica12@gmail.com>
2020-05-18 13:33:05 +0200

    destroy policy

--- demo/destroy.sh
+++ demo/destroy.sh
@@ -3,5 +3,11 @@ terraform destroy -auto-approve \
     -target  azurerm_monitor_diagnostic_setting.psql-diag \
     -target  azurerm_monitor_diagnostic_setting.servicebus-diag \
     -target  azurerm_monitor_diagnostic_setting.appgw-diag \
-    -target  azurerm_monitor_diagnostic_setting.keyvault-diag
+    -target  azurerm_monitor_diagnostic_setting.keyvault-diag \
+    -target  azurerm_policy_assignment.kube-no-privileged-diag \
+    -target  azurerm_policy_assignment.kube-https-ingress \
+    -target  azurerm_policy_assignment.kube-no-public-lb \
+    -target  azurerm_policy_assignment.kube-resource-limits \
+    -target  azurerm_policy_assignment.kube-mandatory-labels \
+    -target  azurerm_policy_assignment.kube-only-acr
 az group delete -n cloudnative-demo -y --no-wait
